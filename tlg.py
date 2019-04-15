d2c54d8780d42990e4c2da55563ecc8cd5f43004
OksanaKostenko <49597340+OksanaKostenko@users.noreply.github.com>
2019-04-15 11:39:57 -0700

    added GET method for fb

--- tlg.py
+++ tlg.py
@@ -39,9 +39,29 @@ def process_update():
         return "ok!", 200
     return "..."
 
+def verify_fb_token(token_sent):
+    #take token sent by facebook and verify it matches the verify token you sent
+    #if they match, allow the request, else return an error 
+    if token_sent == VERIFY_TOKEN:
+        return request.args.get("hub.challenge")
+    return 'Invalid verification token'
+
+#uses PyMessenger to send response to user
+def send_message_fb(recipient_id, response):
+    #sends user the text message provided via input response parameter
+    fb_bot.send_text_message(recipient_id, response)
+    return "success"
+
 @app.route("/", methods=['POST'])
 def receive_message():
-    if request.method == 'POST':
+    if request.method == 'GET':
+        """Before allowing people to message your bot, Facebook has implemented a verify token
+        that confirms all requests that your bot receives came from Facebook.""" 
+        token_sent = request.args.get("hub.verify_token")
+        return verify_fb_token(token_sent)
+    #if the request was not get, it must be POST and we can just proceed with sending a message back to user
+    else:
+        # get whatever message a user sent the bot
        output = request.get_json()
        for event in output['entry']:
           messaging = event['messaging']
@@ -58,13 +78,6 @@ def receive_message():
                     response_sent_nontext = "I'm sorry, I can't handle non text messages"
                     send_message_fb(recipient_id, response_sent_nontext)
     return "Message Processed"
-
-#uses PyMessenger to send response to user
-def send_message_fb(recipient_id, response):
-    #sends user the text message provided via input response parameter
-    fb_bot.send_text_message(recipient_id, response)
-    return "success"
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
